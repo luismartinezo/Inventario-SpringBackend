42ee976c1133e3d3c131a5bd911ac07d68cb442d
Luis Martinez <lmartinezocoro@gmail.com>
2021-02-16 17:04:15 -0500

    Conexion

--- src/main/java/com/co/nexos/prueba/Model/Cargo.java
+++ src/main/java/com/co/nexos/prueba/Model/Cargo.java
@@ -3,10 +3,27 @@
  */
 package com.co.nexos.prueba.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author luis.martinez
- *
+ * @since 16/02/2021
+ * @version 1.0
  */
-public class Cargo {
 
+@Entity
+@Table(name = "cargo")
+public class Cargo {
+	@Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column
+    private String nombre_producto;
 }
